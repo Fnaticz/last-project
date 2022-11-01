4850e3388e436d3bb1fbc7d831902b4ba6d69a85
echa <echaazwaputrialvianti@gmail.com>
2022-11-01 07:27:16 +0700

    21

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,8 +11,8 @@
             <div class="container">
                 <div class="banner_content row">
                     <div class="offset-lg-2 col-lg-8">
-                        <h3>Sewa Lapangan
-                            <br />Online</h3>
+                        <h3>SEWA LAPANGAN
+                            <br />ONLINE</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                             aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                         <a class="white_bg_btn" href="#">View Collection</a>
